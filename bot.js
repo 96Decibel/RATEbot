7b870816c222d4c8d561911097ef3fa184ff6f49
96Decibel <49520814+96Decibel@users.noreply.github.com>
2019-05-17 01:18:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,6 +23,8 @@ client.on('message', message => {
 .addField('→ **$role | Give Role To Some One**')
 .addField('→ **$bc | Send Broadcast**')
 .addField('→ **$new | Create A Ticket**')
+.addField('-:small_blue_diamond: :large_blue_diamond: `Games Commands` :large_blue_diamond: :small_blue_diamond:-')
+.addField('→ **$xo | To Play XO **')
 .setColor('RANDOM')
 message.channel.sendEmbed(embed);
  }
@@ -432,6 +434,72 @@ if (message.content.startsWith(adminprefix + 'setava')) {
     message.channel.send(`Changing The Avatar To :**${argresult}** `);
 }
 });
+
+client.on('message' , message => {
+  var prefix = "$";
+  if(message.author.bot) return;
+ 
+  if(message.content.startsWith(prefix + "xo")) {
+ let array_of_mentions = message.mentions.users.array();
+  let symbols = [':o:', ':heavy_multiplication_x:']
+  var grid_message;
+ 
+  if (array_of_mentions.length == 1 || array_of_mentions.length == 2) {
+    let random1 = Math.floor(Math.random() * (1 - 0 + 1)) + 0;
+    let random2 = Math.abs(random1 - 1);
+    if (array_of_mentions.length == 1) {
+      random1 = 0;
+      random2 = 0;
+    }
+    var player1_id = message.author.id
+    let player2_id = array_of_mentions[random2].id;
+    var turn_id = player1_id;
+    var symbol = symbols[0];
+    let initial_message = `Game match between <@${player1_id}> and <@${player2_id}>!`;
+    if (player1_id == player2_id) {
+      initial_message += '\n_( ألعب مع نفسك)_'
+    }
+    message.channel.send(`Xo ${initial_message}`)
+    .then(console.log("Successful tictactoe introduction"))
+    .catch(console.error);
+    message.channel.send(':one::two::three:' + '\n' +
+                         ':four::five::six:' + '\n' +
+                         ':seven::eight::nine:')
+    .then((new_message) => {
+      grid_message = new_message;
+    })
+    .then(console.log("Successful tictactoe game initialization"))
+    .catch(console.error);
+    message.channel.send('يجب الانتضار حيث ما يتم الموافقه')
+    .then(async (new_message) => {
+      await new_message.react('1⃣');
+      await new_message.react('2⃣');
+      await new_message.react('3⃣');
+      await new_message.react('4⃣');
+      await new_message.react('5⃣');
+      await new_message.react('6⃣');
+      await new_message.react('7⃣');
+      await new_message.react('8⃣');
+      await new_message.react('9⃣');
+      await new_message.react('🆗');
+      await new_message.edit(`It\'s <@${turn_id}>\'s turn! Your symbol is ${symbol}`)
+     .then((new_new_message) => {
+       require('./xo.js')(client, message, new_new_message, player1_id, player2_id, turn_id, symbol, symbols, grid_message);
+     })
+     .then(console.log("Successful tictactoe listener initialization"))
+     .catch(console.error);
+   })
+   .then(console.log("Successful tictactoe react initialization"))
+   .catch(console.error);
+ }
+ else {
+   message.reply(`منشن مع من تريد ألعب`)
+   .then(console.log("Successful error reply"))
+   .catch(console.error);
+ }
+}
+});
+
 
 
 client.login(process.env.BOT_TOKEN);
